8569512278d402391309204c9d297cfeee216079
Nazarii Rybak <newmandnn@gmail.com>
2023-08-25 21:38:16 +0300

    Editing app

--- Flask_Docker/app.py
+++ Flask_Docker/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_760/MTk2NzY3MjA5ODc0MjY5ODI2/top-10-cutest-cat-photos-of-all-time.webp",
+    
     "https://i.pinimg.com/736x/54/f7/48/54f74884dca10f350ce2e445a5c454c8.jpg"
 ]
 
